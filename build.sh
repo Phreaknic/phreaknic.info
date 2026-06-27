0cc460f460ec8f81762551ae715552e95959f99c
Matt <brimstone@the.narro.ws>
2026-06-26 22:36:45 -0400

    fix build script

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -x
 mkdir build
 for f in index.html \
-Orbitron-VariableFont_wght.ttf \
+api \
+fonts \
+images \
+schemas \
 script.js \
-ShareTechMono-Regular.ttf \
-speakers.json \
 styles.css \
-topics.json \
 ; do
-	cp "$f" build/
+	cp -r "$f" build/
 done
